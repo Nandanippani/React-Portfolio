67c7b0726c4b10be43ea8b284ffbdb285b453eca
Nanda Nippani <nandanippani@yahoo.com>
2023-02-04 18:20:00 +0000

    initial code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/header';
+import Nav from './components/nav';
+import projects from './projects.json';
+
+
+
+
+
+function Project({ title, description, github }) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <a href={github}>View on Github</a>
+    </div>
+  );
+}
+
+function Content() {
+  return (
+    <div>
+      {projects.map((project) => (
+        <Project
+          key={project.title}
+          title={project.title}
+          description={project.description}
+          github={project.github}
+        />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Nav />
+      <Content />
     </div>
   );
 }
